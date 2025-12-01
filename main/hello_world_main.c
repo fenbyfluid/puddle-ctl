c747264c2434ebefe1c39affcc22f5994bec8e1c
Erin <fenbyfluid@gmail.com>
2025-12-01 18:51:16 +0000

    Initial USB HID experiments

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdatomic.h>
 #include <string.h>
+#include <fcntl.h>
+#include <errno.h>
 
 #define LOG_LOCAL_LEVEL ESP_LOG_DEBUG
 
@@ -15,10 +17,16 @@
 #include "esp_log.h"
 #include "esp_random.h"
 #include "esp_system.h"
+#include "esp_mac.h"
+#include "esp_check.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "sdkconfig.h"
+#include "esp_vfs.h"
 
+#include "tinyusb_default_config.h"
+#include "tinyusb_cdc_acm.h"
+#include "vfs_tinyusb.h"
 #include "lvgl.h"
 
 #include "esp_lcd_panel_ssd1322.h"
@@ -197,6 +205,7 @@ void display_task_main(void *pvParameters) {
 
     lv_timer_create(update_display_timer, 10, labels);
 
+    // TODO: Implement an exit condition for this task.
     for (;;) {
         uint32_t time_till_next = lv_timer_handler();
         vTaskDelay(pdMS_TO_TICKS(time_till_next));
@@ -248,12 +257,6 @@ void i2c_task_main(void *pvParameters) {
     } else if (i2c_master_probe(bus_handle, 0x43, 100) == ESP_OK) {
         first_encoder = 0x43;
         encoder_count = 1;
-    } else {
-        ESP_LOGW(TAG, "No encoders detected.");
-
-        i2c_del_master_bus(bus_handle);
-        vTaskDelete(NULL);
-        return;
     }
 
     ESP_LOGI(TAG, "Detected first encoder at 0x%02X, count: %d", first_encoder, encoder_count);
@@ -283,6 +286,10 @@ void i2c_task_main(void *pvParameters) {
         ESP_ERROR_CHECK(i2c_encoder_set_led_color(encoder_handles[i], 0x7F / led_scale, 0xFF / led_scale, 0x7F / led_scale));
     }
 
+    bool hid_report_changed = false;
+    uint8_t hid_report[CFG_TUD_HID_EP_BUFSIZE - 1] = {0};
+    
+    // TODO: Implement an exit condition for this task.
     for (;;) {
         for (int i = 0; i < encoder_count; ++i) {
             // ESP_ERROR_CHECK(i2c_encoder_poll_status(encoder_handles[i]));
@@ -291,6 +298,10 @@ void i2c_task_main(void *pvParameters) {
             ESP_ERROR_CHECK(i2c_encoder_read_value(encoder_handles[i], &value));
 
             encoder_values[i] = value;
+            if (value != hid_report[i]) {
+                hid_report[i] = value;
+                hid_report_changed = true;
+            }
 
             for (uint8_t j = 0; j < IS31FL3746A_LED_COUNT; ++j) {
                 if (value > j) {
@@ -301,6 +312,11 @@ void i2c_task_main(void *pvParameters) {
             }
 
             ESP_ERROR_CHECK(is31fl3746a_flush_led_color(ring_handles[i]));
+        }
+
+        if (hid_report_changed && tud_hid_ready()) {
+            tud_hid_report(1, hid_report, sizeof(hid_report));
+            hid_report_changed = false;
         }
 
         vTaskDelay(pdMS_TO_TICKS(10));
@@ -324,10 +340,339 @@ void i2c_task_main(void *pvParameters) {
     vTaskDelete(NULL);
 }
 
+static void device_event_handler(tinyusb_event_t *event, void *arg){
+    ESP_LOGD(TAG, "USB event: %d", event->id);
+}
+
+const uint8_t hid_report_descriptor[] = {
+    TUD_HID_REPORT_DESC_GENERIC_INOUT(CFG_TUD_HID_EP_BUFSIZE - 1, HID_REPORT_ID(1)),
+};
+
+#define TUSB_DESC_TOTAL_LEN (TUD_CONFIG_DESC_LEN + (1 * TUD_HID_INOUT_DESC_LEN) + (1 * TUD_CDC_DESC_LEN))
+
+/**
+ * @brief Configuration descriptor
+ *
+ * This is a simple configuration descriptor that defines 1 configuration and 1 HID interface
+ */
+static const uint8_t hid_configuration_descriptor[] = {
+    // Configuration number, interface count, string index, total length, attribute, power in mA
+    TUD_CONFIG_DESCRIPTOR(1, 2, 0, TUSB_DESC_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 500),
+
+    // Interface number, string index, boot protocol, report descriptor len, EP data address (out, in), size, and polling interval
+    TUD_HID_INOUT_DESCRIPTOR(0, 4, false, sizeof(hid_report_descriptor), 0x01, 0x81, CFG_TUD_HID_EP_BUFSIZE, 10),
+
+    // Interface number, string index, EP notification address and size, EP data address (out, in), and size.
+    TUD_CDC_DESCRIPTOR(1, 5, 0x82, 8, 0x03, 0x83, CFG_TUD_ENDPOINT0_SIZE),
+};
+
+// Invoked when received GET HID REPORT DESCRIPTOR request
+// Application return pointer to descriptor, whose contents must exist long enough for transfer to complete
+uint8_t const *tud_hid_descriptor_report_cb(uint8_t instance) {
+    ESP_LOGD(TAG, "tud_hid_descriptor_report_cb(%d)", instance);
+
+    return hid_report_descriptor;
+}
+
+// Invoked when received GET_REPORT control request
+// Application must fill buffer report's content and return its length.
+// Return zero will cause the stack to STALL request
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer, uint16_t reqlen) {
+    ESP_LOGD(TAG, "tud_hid_get_report_cb(%d, %d, %d, %p, %d)", instance, report_id, report_type, buffer, reqlen);
+    
+    (void) instance;
+    (void) report_id;
+    (void) report_type;
+    (void) buffer;
+    (void) reqlen;
+
+    if (report_id == 1 && report_type == HID_REPORT_TYPE_INPUT && reqlen >= 4) {
+        buffer[0] = encoder_values[0];
+        buffer[1] = encoder_values[1];
+        buffer[2] = encoder_values[2];
+        buffer[3] = encoder_values[3];
+
+        return 4;
+    }
+
+    // report_id is automatically prefixed if non-zero.
+    // TinyUSB will assert if the total length of the response is zero (i.e. we return 0 bytes for report ID 0).
+    return 0;
+}
+
+// Invoked when received SET_REPORT control request or
+// received data on OUT endpoint ( Report ID = 0, Type = 0 )
+// 
+// When confused why the first byte is missing: https://github.com/hathach/tinyusb/issues/2929
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer, uint16_t bufsize) {
+    ESP_LOGD(TAG, "tud_hid_set_report_cb(%d, %d, %d, %p, %d)", instance, report_id, report_type, buffer, bufsize);
+
+    ESP_LOG_BUFFER_HEX_LEVEL(TAG, buffer, bufsize, ESP_LOG_DEBUG);
+
+    (void) instance;
+    (void) report_id;
+    (void) report_type;
+    (void) buffer;
+    (void) bufsize;
+}
+
+// The rest of these are optional callbacks.
+
+// Invoked when received SET_IDLE request. return false will stall the request
+// - Idle Rate = 0 : only send report if there is changes, i.e. skip duplication
+// - Idle Rate > 0 : skip duplication, but send at least 1 report every idle rate (in unit of 4 ms).
+bool tud_hid_set_idle_cb(uint8_t instance, uint8_t idle_rate) {
+    ESP_LOGD(TAG, "tud_hid_set_idle_cb(%d, %d)", instance, idle_rate);
+
+    (void) instance;
+    (void) idle_rate;
+
+    return true;
+}
+
+// Invoked when sent REPORT successfully to host
+// Application can use this to send the next report
+// Note: For composite reports, report[0] is report ID
+void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report, uint16_t len) {
+    // ESP_LOGD(TAG, "tud_hid_report_complete_cb(%d, %p, %d)", instance, report, len);
+
+    // ESP_LOG_BUFFER_HEX_LEVEL(TAG, report, len, ESP_LOG_DEBUG);
+
+    (void) instance;
+    (void) report;
+    (void) len;
+}
+
+// Invoked when a transfer wasn't successful
+void tud_hid_report_failed_cb(uint8_t instance, hid_report_type_t report_type, uint8_t const* report, uint16_t xferred_bytes) {
+    ESP_LOGD(TAG, "tud_hid_report_failed_cb(%d, %d, %p, %d)", instance, report_type, report, xferred_bytes);
+
+    ESP_LOG_BUFFER_HEX_LEVEL(TAG, report, xferred_bytes, ESP_LOG_DEBUG);
+
+    (void) instance;
+    (void) report_type;
+    (void) report;
+    (void) xferred_bytes;
+}
+
+// Placeholder, will be replaced with chip ID
+char serial_number[32] = CONFIG_TINYUSB_DESC_SERIAL_STRING;
+
+const char *descriptor_strings[] = {
+    (char[]){0x09, 0x04},                    // 0: is supported language is English (0x0409)
+    CONFIG_TINYUSB_DESC_MANUFACTURER_STRING, // 1: Manufacturer
+    CONFIG_TINYUSB_DESC_PRODUCT_STRING,      // 2: Product
+    serial_number,                           // 3: Serials, should use chip ID
+    CONFIG_TINYUSB_DESC_PRODUCT_STRING,      // 4: HID Interface
+    CONFIG_TINYUSB_DESC_CDC_STRING,          // 5: CDC Interface
+    NULL                                     // NULL: Must be last. Indicates end of array
+};
+
+void usb_task_cleanup() {
+    freopen("/dev/console", "w", stdout);
+    freopen("/dev/console", "w", stderr);
+
+    esp_vfs_tusb_cdc_unregister(TINYUSB_CDC_ACM_0);
+
+    tinyusb_cdcacm_deinit(TINYUSB_CDC_ACM_0);
+
+    ESP_ERROR_CHECK(tinyusb_driver_uninstall());
+}
+
+#include "esp_private/usb_phy.h"
+#include "soc/rtc_cntl_reg.h"
+#include "hal/usb_serial_jtag_hal.h"
+#include "esp32s3/rom/usb/usb_dc.h"
+#include "esp32s3/rom/usb/usb_persist.h"
+#include "esp32s3/rom/usb/chip_usb_dw_wrapper.h"
+
+#include "soc/usb_serial_jtag_reg.h"
+
+// TODO: This isn't the best experience, as the USB PID changes when we restart, which causes it to get detected as a different serial port.
+//       Investigate implementing USB DFU mode and doing it as an OTA update instead.
+//       We should get ourselves a pair of custom PIDs from Espressif, and do both in USB-OTG mode.
+void restart_to_bootloader(void *pvParameters) {
+    ESP_LOGW(TAG, "Preparing to restart to bootloader");
+
+    // TODO: Make sure the USB task is stopped before we do this. This is a bit of a hack for now.
+    // TODO: Make sure we're not in the middle of sending a HID report either.
+    usb_task_cleanup();
+
+    vTaskDelay(pdMS_TO_TICKS(1000));
+
+    // Switch the USB PHY back to Serial/Jtag mode, disabling OTG support.
+    usb_phy_handle_t phy_hdl;
+    usb_phy_config_t phy_conf = {
+        .controller = USB_PHY_CTRL_SERIAL_JTAG,
+    };
+    ESP_ERROR_CHECK(usb_new_phy(&phy_conf, &phy_hdl));
+
+    // Tell the USB peripheral we're going to enter ROM DFU mode.
+    usb_dc_prepare_persist();
+    chip_usb_set_persist_flags(USBDC_PERSIST_ENA);
+    // chip_usb_set_persist_flags(USBDC_BOOT_DFU);
+
+    // Tell the bootloader to enter download mode.
+    REG_WRITE(RTC_CNTL_OPTION1_REG, RTC_CNTL_FORCE_DOWNLOAD_BOOT);
+    
+    ESP_LOGI(TAG, "Restarting...");
+
+    // Reset the system
+    esp_restart();
+}
+
+void cdc_line_state_changed(int itf, cdcacm_event_t *event) {
+    if (event->type == CDC_EVENT_LINE_CODING_CHANGED) {
+        ESP_LOGI(TAG, "CDC line coding changed: %lu %u %u %u",
+             event->line_coding_changed_data.p_line_coding->bit_rate,
+             event->line_coding_changed_data.p_line_coding->data_bits,
+             event->line_coding_changed_data.p_line_coding->parity,
+             event->line_coding_changed_data.p_line_coding->stop_bits);
+
+        return;
+    }
+
+    if (event->type != CDC_EVENT_LINE_STATE_CHANGED) {
+        return;
+    }
+
+    static bool prev_dtr = false;
+    static bool prev_rts = false;
+
+    bool dtr = event->line_state_changed_data.dtr;
+    bool rts = event->line_state_changed_data.rts;
+
+    ESP_LOGI(TAG, "CDC line state changed: DTR = %d, RTS = %d", dtr, rts);
+
+    static bool is_restarting = false;
+    if (dtr && !rts && !prev_dtr && prev_rts && !is_restarting) {
+        is_restarting = true;
+        xTaskCreatePinnedToCore(restart_to_bootloader, "restart_to_bootloader", 4096, NULL, 5, NULL, xPortGetCoreID());
+    }
+
+    prev_dtr = dtr;
+    prev_rts = rts;
+}
+
+static ssize_t myfs_write(int fd, const void *data, size_t size) {
+    static _lock_t myfs_write_lock;
+    static FILE *console_stream = NULL;
+    static FILE *tusb_cdc_stream = NULL;
+
+    _lock_acquire(&myfs_write_lock);
+
+    if (!console_stream) {
+        console_stream = fopen("/dev/console", "w");
+        assert(console_stream != NULL && "Failed to open console stream");
+
+        fputs("Console stream initialized.\n", console_stream);
+    }
+
+    if (!tusb_cdc_stream) {
+        tusb_cdc_stream = fopen("/dev/tusb_cdc", "w");
+        assert(tusb_cdc_stream != NULL && "Failed to open USB CDC stream");
+
+        fputs("USB CDC stream initialized.\n", tusb_cdc_stream);
+    }
+
+    // We've seen the USB CDC stream get write blocked, whereas the console stream appears to buffer.
+    // To avoid losing data, we only write to the console stream bytes that were successfully written to the USB CDC stream.
+    // This is a lot simpler than two-way synchronization.
+    // We return the number of bytes written to the console stream, which is the minimum of the two streams.
+    // If the console stream ever fell behind, we'd write duplicate data to USB CDC, and would need the more complex logic.
+
+    size_t tusb_cdc_written = fwrite(data, 1, size, tusb_cdc_stream);
+
+    size_t console_written = fwrite(data, 1, tusb_cdc_written, console_stream);
+
+    _lock_release(&myfs_write_lock);
+
+    return console_written;
+}
+
+static ssize_t myfs_open(const char *path, int flags, int mode) {
+    return 0;
+}
+
+static int myfs_close(int fd) {
+    return 0;
+}
+
+static const esp_vfs_fs_ops_t myfs = {
+    .write = &myfs_write,
+    .open = &myfs_open,
+    .close = &myfs_close,
+};
+
+void usb_task_main(void *pvParameters) {
+    uint8_t mac[6];
+    ESP_ERROR_CHECK(esp_read_mac(mac, ESP_MAC_EFUSE_FACTORY));
+    snprintf(serial_number, sizeof(serial_number), "%02x:%02x:%02x:%02x:%02x:%02x",
+        mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+    tinyusb_config_t tusb_cfg = TINYUSB_DEFAULT_CONFIG(device_event_handler);
+
+    tusb_cfg.descriptor.full_speed_config = hid_configuration_descriptor;
+#if (TUD_OPT_HIGH_SPEED)
+    tusb_cfg.descriptor.high_speed_config = hid_configuration_descriptor;
+#endif // TUD_OPT_HIGH_SPEED
+
+    tusb_cfg.descriptor.string = descriptor_strings;
+    while (tusb_cfg.descriptor.string[++tusb_cfg.descriptor.string_count] != NULL);
+
+    ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
+
+    const tinyusb_config_cdcacm_t acm_cfg = {
+        .cdc_port = TINYUSB_CDC_ACM_0,
+        .callback_rx = NULL,
+        .callback_rx_wanted_char = NULL,
+        .callback_line_state_changed = cdc_line_state_changed,
+        .callback_line_coding_changed = cdc_line_state_changed,
+    };
+    ESP_ERROR_CHECK(tinyusb_cdcacm_init(&acm_cfg));
+
+    // This replaces the default console(s), we want to mirror instead.
+    // We want output going to UART0, JTAG, and USB CDC.
+    // We do this with a custom VFS that writes to both the console and USB CDC streams.
+    // ESP_ERROR_CHECK(esp_tusb_init_console(TINYUSB_CDC_ACM_0));
+
+    ESP_ERROR_CHECK(esp_vfs_tusb_cdc_register(TINYUSB_CDC_ACM_0, NULL));
+
+    ESP_ERROR_CHECK(esp_vfs_register_fs("/dev/myconsole", &myfs, ESP_VFS_FLAG_STATIC, NULL));
+
+    freopen("/dev/myconsole", "w", stdout);
+    setlinebuf(stdout);
+    freopen("/dev/myconsole", "w", stderr);
+    setlinebuf(stderr);
+
+    ESP_LOGI(TAG, "Console mirrored to USB CDC");
+
+    // TODO: Considering performance, do we actually want to be using USB-CDC for the logging?
+    //       Minimal latency is quite critical, so we need to ensure this doesn't have any contention with the HID interface.
+    //       It's just a little frustrating for debugging if we need a phyiscal UART connection to see the logs.
+
+    // TODO: Implement an exit condition for this task.
+    for (;;) {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
+        // Send a dummy report to test even when we have no encoders.
+        // uint8_t hid_report[CFG_TUD_HID_EP_BUFSIZE - 1] = {0xCA};
+        // tud_hid_report(1, hid_report, sizeof(hid_report));
+    }
+
+    usb_task_cleanup();
+
+    vTaskDelete(NULL);
+}
+
 void app_main(void) {
     esp_log_level_set(TAG, ESP_LOG_DEBUG);
 
-    printf("Hello world!\n");
+    ESP_LOGI(TAG, "Hello world!");
+
+    // TODO: If the last reset was due to a panic, we may not want to continue automatically.
+    esp_reset_reason_t reset_reason = esp_reset_reason();
+    ESP_LOGI(TAG, "Reset reason: %d", reset_reason);
 
     ESP_ERROR_CHECK(gpio_install_isr_service(0));
 
@@ -337,15 +682,16 @@ void app_main(void) {
     //       Need to understand why that is and how to safely reset it - is it enough to bring it low or are the I2C/SPI peripherals also still configured?
 
     // for (int i = 10; i >= 0; i--) {
-    //     printf("Waiting %d seconds...\n", i);
+    //     ESP_LOGI(TAG, "Waiting %d seconds...", i);
     //     vTaskDelay(pdMS_TO_TICKS(1000));
     // }
 
     // The on-board I2C pull-up resistors are tied to VSENSOR. Fortunately we can spare the current now.
     enable_stemma_power(true);
 
-    xTaskCreatePinnedToCore(display_task_main, "OLED", 8192, NULL, 10, NULL, xPortGetCoreID());
-    xTaskCreatePinnedToCore(i2c_task_main, "I2C", 8192, NULL, 10, NULL, xPortGetCoreID());
+    xTaskCreatePinnedToCore(display_task_main, "OLED", 8192, NULL, 1, NULL, xPortGetCoreID());
+    xTaskCreatePinnedToCore(i2c_task_main, "I2C", 8192, NULL, 1, NULL, xPortGetCoreID());
+    xTaskCreatePinnedToCore(usb_task_main, "USB", 8192, NULL, 1, NULL, xPortGetCoreID());
 
     // Print heap memory usage statistics every 10 seconds
     for (;;) {
@@ -361,7 +707,7 @@ void app_main(void) {
             total_heap, free_heap, min_free_heap, used_percent);
     }
 
-    // printf("Restarting now.\n");
+    // ESP_LOGI(TAG, "Restarting now.");
     // fflush(stdout);
     // esp_restart();
 }
